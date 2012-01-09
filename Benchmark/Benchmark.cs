5b7650dfdc2052152150140020f4968df41839a7
Mike Reust <reustmd@gmail.com>
2012-01-09 00:10:24 -0500

    Added helper class for benchmarking funcs

---
+++ Benchmark/Benchmark.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Benchmark
+{
+	public class Benchmark
+	{
+		public static TimeSpan MeasureExecutionTime(Func<int> func)
+		{
+			var stopwatch = Stopwatch.StartNew();
+			func();
+			return stopwatch.Elapsed;
+		}
+
+		public static void ExecuteAndPrintExecutionTimeToConsole(Action action)
+		{
+			var executionTime = ExecuteAndReturnExecutionTime(action);
+			Console.WriteLine(string.Format("Action took {0} seconds", executionTime.TotalSeconds));
+		}
+
+		public static TimeSpan ExecuteAndReturnExecutionTime(Action action)
+		{
+			var timer = new Stopwatch();
+			timer.Start();
+
+			action.Invoke();
+
+			timer.Stop();
+			return timer.Elapsed;
+		}
+
+		public static IEnumerable<TimeSpan> ExecuteMultipleTimesAndReturnTimeSpans(Action action, int numRuns, Action actionToPerformEachIteration = null)
+		{
+			var timeSpans = new List<TimeSpan>();
+
+			for (int i = 0; i < numRuns; i++)
+			{
+				timeSpans.Add(ExecuteAndReturnExecutionTime(action));
+				if (actionToPerformEachIteration != null) actionToPerformEachIteration.Invoke();
+			}
+
+			return timeSpans;
+		}
+
+		public static void ExecuteMultipleTimesAndPrintSummaryToConsole(Action action, int numRuns)
+		{
+			int runNumber = 0;
+			var runTimes = ExecuteMultipleTimesAndReturnTimeSpans(action, numRuns, () => Console.WriteLine(string.Format("Run {0} completed", ++runNumber)))
+								.Select(x => x.TotalSeconds);
+
+			var summary = new MultipleRunSummary { AverageRuntimeInSeconds = runTimes.Average(), MedianRuntimeInSeconds = runTimes.Median() };
+			Console.WriteLine("Action executed {0} times, average runtime in seconds: {1}, median runtime in seconds {2}", numRuns, summary.AverageRuntimeInSeconds, summary.MedianRuntimeInSeconds);
+		}
+
+		private class MultipleRunSummary
+		{
+			public double AverageRuntimeInSeconds { get; set; }
+			public double MedianRuntimeInSeconds { get; set; }
+		}
+	}
+}
---
+++ Benchmark/CollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Benchmark
+{
+	public static class CollectionExtensions
+	{
+		public static double Median(this IEnumerable<double> source)
+		{
+			if (source == null || !source.Any())
+				throw new ArgumentException("Cannot calculate median unless source contains at least 1 element");
+
+			var sortedList = source.OrderBy(x => x);
+			int itemIndex = sortedList.Count() / 2;
+
+			if (sortedList.Count() % 2 == 0)
+				return (sortedList.ElementAt(itemIndex) + sortedList.ElementAt(itemIndex - 1)) / 2;
+
+			return sortedList.ElementAt(itemIndex);
+		}		
+	}
+}
